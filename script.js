89210ba6d0983a095c475a64bde8107e3357b112
princePandeyGitHub <princecode13@gmail.com>
2024-09-05 11:13:52 +0530

    Update script.js
    made it more advanced

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.getElementById('user-form').addEventListener('submit',(event)=>{
     event.preventDefault();
     const userName = document.getElementById('user-name').value;
     const userAge = document.getElementById('user-age').value;
+    const userAddress = document.getElementById('user-address').value;
+    const userGender = document.getElementById('user-gender').value;
+    const userEducation = document.getElementById('user-education').value;
+    const userProjects = document.getElementById('user-projects').value;
     const userSkill = document.getElementById('user-skills').value;
     const userDescription = document.getElementById('user-description').value;
     const userExperience = document.getElementById('user-experience').value;
@@ -9,14 +13,26 @@ document.getElementById('user-form').addEventListener('submit',(event)=>{
     const userEmail = document.getElementById('user-email').value;
 
     const output = `<h1 class="heading">${userName}</h1>
-    <p>Description:${userDescription}</p>
+    <p>Description:${userDescription.replace(/\n/g, '<br>')}</p>
     <hr>
+    <h3>Personal Information</h3>
     <h4 class="resume-output">Age: ${userAge}</h4>
-    <h4 class="resume-output">Skills: ${userSkill}</h4>
-    <h4 class="resume-output">Experience: ${userExperience}</h4>
+    <h4 class="resume-output">Gender: ${userGender}</h4>
+    <h4 class="resume-output">Address: ${userAddress}</h4>
     <h4 class="resume-output">Contact Number: ${userNumber}</h4>
     <h4 class="resume-output">Email id: ${userEmail}</h4>
+    <hr>
+    <h3>Skills and Experience</h3>
+    <h4 class="resume-output">Skills: ${userSkill.replace(/\n/g, '<br>')}</h4>
+    <h4 class="resume-output">Experience: ${userExperience}</h4>
+    <hr>
+    <h3>Education</h3>
+    <h4 class="resume-output">${userEducation.replace(/\n/g, '<br>')}</h4>
+    <hr>
+    <h3>Projects</h3>
+    <h4 class="resume-output">${userProjects.replace(/\n/g, '<br>')}</h4>
     <input type="button" value="Download Resume" class="button" id="download">
+    <hr>
     `
     document.getElementById('resume-output').innerHTML = output;
 
